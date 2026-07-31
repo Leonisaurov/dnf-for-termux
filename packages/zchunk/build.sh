dbc391d1819df4c3a7c6fa4b86ed4d5a3cf47621
Leonisaurov <leonisaurov@gmail.com>
2026-07-30 22:38:09 -0600

    fix: use zstd (not libzstd) in zchunk deps (buildorder)

--- packages/zchunk/build.sh
+++ packages/zchunk/build.sh
@@ -5,5 +5,5 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.5.3
 TERMUX_PKG_SRCURL=https://github.com/zchunk/zchunk/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=832381dafe192109742c141ab90a6bc0a9d7e9926a4bafbdf98f596680da2a95
-TERMUX_PKG_DEPENDS="zlib, libzstd, openssl, libcurl"
+TERMUX_PKG_DEPENDS="zlib, zstd, openssl, libcurl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dwith-zstd=enabled -Dwith-openssl=enabled -Dwith-curl=enabled -Dtests=false -Ddocs=false"
